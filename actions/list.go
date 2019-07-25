d69759515b0455817ca483375fde262d7e9accc8
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-07-25 01:52:29 -0500

    Fix bug cycle imports

--- actions/list.go
+++ actions/list.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-
-	"github.com/Obsinqsob01/reto-ed/utils"
 )
 
 // APIBase es el endpoint del api
@@ -14,7 +12,9 @@ const APIBase = "https://jsonplaceholder.typicode.com/photos"
 // List obtiene todas las fotos del api y las muestra
 func List() {
 	response, err := http.Get(APIBase)
-	utils.CheckErr(err, "Ocurrió un error al tratar de llamar al api")
+	if err != nil {
+		panic(err)
+	}
 
 	data, _ := ioutil.ReadAll(response.Body)
 
